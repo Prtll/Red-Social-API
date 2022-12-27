6c4f65c41e6cbb4d774a124c08f0c356dfab1ec2
David Portilla <david77fpm@gmail.com>
2022-12-26 22:07:06 -0500

    modifie mail

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -6,7 +6,7 @@ const transporter = nodemailer.createTransport({
     port: 465,
     secure: true,
     auth: {
-        user: 'test.academlo@gmail.com',
+        user: 'david77fpm@gmail.com',
         pass: config.api.emailPass
     }
 })
